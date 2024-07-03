d1167a3a42ed1fea9cc94399d119468baca8f567
Mateus V Gasparino <matvalverde@gmail.com>
2024-07-03 15:21:12 +0000

    DLIO is working

--- README.md
+++ README.md
@@ -4,16 +4,42 @@ A package with everything you need to run traversability algorithms on TerraSent
 ## Setup
 
 This repository is created to run on a custom modification of the TerraSentia robot.
-
-![outline](images/terrasentia_new.png)
+<p align="center">
+    <img src="images/terrasentia_new.png" width="400"/>
+</p>
 
 Our TerraSentia mod, referred as TerraSentia*, contains a Velodyne Puck VLP-16 LiDAR and three fixed RGB cameras.
 
-### To use DLIO as the state estimator
+### Time synchronization in multiple devices
+To ensure the IMU messages are synchronized with the other messages, install chrony in the Raspberry Pi device (192.168.1.135):
+```
+sudo apt-get install chrony
+```
+Choose one machine as time server (usually the Jetson 192.168.1.99).
+On this machine, do:
+```
+sudo vim /etc/chrony/chrony.conf
+```
+Add these lines:
+```
+# make it serve time even if it is not synced (as it can't reach out)
+local stratum 8
+# allow the IP of your peer to connect
+allow 192.168.1.135
+```
+Then, on the client (`192.168.1.133`):
+```
+sudo vim /etc/chrony/chrony.conf
+```
+Add this lines:
+```
+server 192.168.1.99 minpoll 0 maxpoll 5 maxdelay .05
+
+## To use DLIO as the state estimator
 
 Clone the DLIO package (our fork):
 ```shell
-git clone https://github.com/matval/direct_lidar_inertial_odometry
+git clone git@github.com:matval/direct_lidar_inertial_odometry.git
 ```
 ### Run the navigation package
 - Make sure the package is compiled. In your ROS workspace, run:
--- scripts/imu_conversion.py
+++ scripts/imu_conversion.py
@@ -2,6 +2,7 @@
 import rospy
 import numpy as np
 
+from collections import deque
 from sensor_msgs.msg import Imu
 
 class IMUConversion(object):
@@ -12,23 +13,54 @@ class IMUConversion(object):
         self._imu_sub = rospy.Subscriber("/controller/imu", Imu, self.receiveIMU)
         self._imu_pub = rospy.Publisher("/terrasentia/imu", Imu, queue_size=1)
 
+        window_size = 5
+        self.accel_x = deque(maxlen=window_size)
+        self.accel_y = deque(maxlen=window_size)
+        self.accel_z = deque(maxlen=window_size)
+        self.gyro_x = deque(maxlen=window_size)
+        self.gyro_y = deque(maxlen=window_size)
+        self.gyro_z = deque(maxlen=window_size)
+        self.rot_x = deque(maxlen=window_size)
+        self.rot_y = deque(maxlen=window_size)
+        self.rot_z = deque(maxlen=window_size)
+        self.rot_w = deque(maxlen=window_size)
+
     def receiveIMU(self, msg):
         imu_msg = Imu()
         imu_msg.header = msg.header
-
         imu_msg.header.frame_id = "imu_link"
-        # imu_msg.header.stamp = rospy.Time.now()
 
-        # Changing the signal of the IMU
-        imu_msg.orientation = msg.orientation
+        # Store new data in sliding window
+        self.accel_x.append(msg.linear_acceleration.x)
+        self.accel_y.append(msg.linear_acceleration.y)
+        self.accel_z.append(msg.linear_acceleration.z)
+        self.gyro_x.append(msg.angular_velocity.x)
+        self.gyro_y.append(msg.angular_velocity.y)
+        self.gyro_z.append(msg.angular_velocity.z)
+        self.rot_x.append(msg.orientation.x)
+        self.rot_y.append(msg.orientation.y)
+        self.rot_z.append(msg.orientation.z)
+        self.rot_w.append(msg.orientation.w)
 
-        imu_msg.angular_velocity.x = msg.angular_velocity.x
-        imu_msg.angular_velocity.y = msg.angular_velocity.y
-        imu_msg.angular_velocity.z = msg.angular_velocity.z
+        # Publish the new filtered IMU msg
+        imu_msg.angular_velocity.x = np.median(self.gyro_x)
+        imu_msg.angular_velocity.y = np.median(self.gyro_y)
+        imu_msg.angular_velocity.z = np.median(self.gyro_z)
+        imu_msg.linear_acceleration.x = -np.median(self.accel_x)
+        imu_msg.linear_acceleration.y = -np.median(self.accel_y)
+        imu_msg.linear_acceleration.z = -np.median(self.accel_z)
+        new_quat = np.array([
+            np.median(self.rot_x),
+            np.median(self.rot_y),
+            np.median(self.rot_z),
+            np.median(self.rot_w)
+        ])
 
-        imu_msg.linear_acceleration.x = -msg.linear_acceleration.x #* 0.981
-        imu_msg.linear_acceleration.y = -msg.linear_acceleration.y #* 0.981
-        imu_msg.linear_acceleration.z = -msg.linear_acceleration.z #* 0.981
+        new_quat /= np.linalg.norm(new_quat)
+        imu_msg.orientation.x = new_quat[0]
+        imu_msg.orientation.y = new_quat[1]
+        imu_msg.orientation.z = new_quat[2]
+        imu_msg.orientation.w = new_quat[3]
         
         self._imu_pub.publish(imu_msg)
 
